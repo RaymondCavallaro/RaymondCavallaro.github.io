7c07eb167d1ffb3e13e41f3aa5004655b6a57814
raymond <raymond.cavallaro@gmail.com>
2020-05-28 07:53:02 +0000

    -

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 module.exports = {
-	entry : './src/index.js',
+	entry : {
+		'index': './src/index.js'
+	},
 	output : {
-		filename : 'main.js',
-		path : path.resolve(__dirname, 'dist'),
+		filename : '[name].bundle.js',
+		path : path.resolve(__dirname, 'build'),
 	},
 };
